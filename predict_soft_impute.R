38f8d7db1b14968b5fc94b241cf0e9a04c1ae931
Christopher Jenness <christopherjenness@gmail.com>
2016-11-21 06:05:51 -0500

    Add documentation and clean

---
+++ predict_soft_impute.R
@@ -0,0 +1,32 @@
+library('softImpute')
+
+# Load and organize pace and offensive rating data
+df.pace = read.csv('pace.csv')
+df.OR = read.csv('OR.csv')
+
+df.pace[df.pace == 0] <- NA
+df.OR[df.OR==0] = NA
+
+df.pace = as.matrix(df.pace)
+df.OR = as.matrix(df.OR)
+
+df.pace = df.pace[,seq(2, 31)]
+df.OR = df.OR[,seq(2, 31)]
+
+class(df.pace) = 'numeric'
+class(df.OR) = 'numeric'
+
+# Soft-impute data
+fits.pace = softImpute(x=df.pace,trace=TRUE, type="svd", lambda=20, rank.max = 5)
+pace = fits.pace$u %*% diag(fits.pace$d) %*% t(fits.pace$v)
+
+fits.OR=softImpute(x=df.OR,trace=TRUE, type="svd", lambda=20, rank.max = 5)
+OR = fits.OR$u %*% diag(fits.OR$d) %*% t(fits.OR$v)
+
+# Compute and organize predictions
+predictions = OR * pace / 100
+
+colnames(predictions) = colnames(df)
+rownames(predictions) = colnames(df)
+
+write.csv(predictions, 'predictions.csv')
